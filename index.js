c35bbd2c923de1463412d033870597acad2d02a5
Eric Dudley <ericdudley@email.com>
2018-01-18 21:49:29 -0500

    Implemented crud endpoints.

--- index.js
+++ index.js
@@ -1,13 +1,88 @@
 'use strict';
 
-var express = require('express');
+var express = require('express')
+var Realm = require('realm')
+var bodyParser = require('body-parser')
 
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send("Hello World!");
-});
+app.use(bodyParser.urlencoded({extended: true}))
+
+let PostSchema = {
+  name: 'Post',
+  primaryKey: 'timestamp',
+  properties: {
+    timestamp: 'int',
+    title: 'string',
+    content: 'string'
+  }
+}
+
+var blogRealm = new Realm({
+  path: 'blog.realm',
+  schema: [PostSchema]
+})
+
+function getPostByTimestamp(timestamp){
+  return blogRealm.objects('Post').filtered('timestamp = '+timestamp.toString())
+}
+
+app.post('/', function(req, res){
+  let 
+  title = req.body['title'],
+  content = req.body['content'],
+  timestamp = Date.now()
+
+  blogRealm.write(() => {
+    blogRealm.create('Post',{
+      title: title,
+      content: content,
+      timestamp: timestamp
+    })
+  })
+  res.send(getPostByTimestamp(timestamp))
+})
+
+app.get('/', function(req, res){
+  res.send(blogRealm.objects('Post').sorted('timestamp', true))
+})
+
+app.get('/:timestamp', function(req, res){
+  let timestamp = req.params.timestamp
+  res.send(getPostByTimestamp(timestamp))  
+})
+
+app.put('/:timestamp', function(req, res){
+  let 
+  title = req.body['title'],
+  content = req.body['content'],
+  timestamp = parseInt(req.params.timestamp)
+
+  if(getPostByTimestamp(timestamp)){
+    blogRealm.write(() => {
+      blogRealm.create('Post',{
+        title: title,
+        content: content,
+        timestamp: timestamp
+      }, true)
+    })
+  }
+  res.send(getPostByTimestamp(timestamp))  
+})
+
+app.delete('/:timestamp', function(req, res){
+  let 
+  timestamp = req.params.timestamp,
+  post = getPostByTimestamp(timestamp)
+
+  if(post){
+    blogRealm.write(() => {
+      blogRealm.delete(post)
+    })
+  }
+  res.send('')
+})
 
 app.listen(3000, function() {
-  console.log("Go!");
+  console.log("Blog server running...")
 });
